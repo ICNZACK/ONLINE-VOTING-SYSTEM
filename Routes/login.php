a09a04c06e77459fea3fe8b21c48fd55ea58f6c7
Sourav Ghosh <souravghosh007980@gmail.com>
2023-11-02 13:56:15 +0530

    close icon removed

--- Routes/login.php
+++ Routes/login.php
@@ -23,7 +23,6 @@
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
     <div class="wrapper">
-        <span class="icon-close"><ion-icon name="close"></ion-icon></span>
         <div class="form-box login">
             <h2>Login</h2>
             <form action="#">
