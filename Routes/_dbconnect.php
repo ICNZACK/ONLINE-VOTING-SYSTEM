c096a8f840220be75461f9408adb1e00754354a4
Sourav Ghosh <souravghosh007980@gmail.com>
2023-11-03 00:34:44 +0530

    database connected

---
+++ Routes/_dbconnect.php
@@ -0,0 +1,15 @@
+<?php
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "voting_database";
+
+$conn = mysqli_connect($server, $username, $password, $database);
+
+if($conn){
+    echo "connected";
+}
+else{
+    die("Error".mysqli_connect_error());
+}
+?>
