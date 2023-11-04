21cfd66737a6c79cf86f39bec324f7a173effae6
Sourav Ghosh <souravghosh007980@gmail.com>
2023-11-04 11:40:00 +0530

    dashboard

---
+++ Routes/dashboard.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <button>Back</button>
+    <button>Logout</button>
+    <h1>Online Voting system</h1>
+    <hr>
+    <div class="profile"></div>
+    <div class="group"></div>
+</body>
+</html>
