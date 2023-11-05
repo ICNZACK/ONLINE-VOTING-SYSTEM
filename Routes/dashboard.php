44f29598c6b3ce6537f0dfeb69e808548656fa6c
Sourav Ghosh <souravghosh007980@gmail.com>
2023-11-05 11:45:54 +0530

    profile add in dashboard

--- Routes/dashboard.php
+++ Routes/dashboard.php
@@ -3,6 +3,8 @@
     if(!isset($_SESSION['userdata'])){
         header("location: login.html");
     }
+
+    $userdata = $_SESSION['userdata'];
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,34 +12,60 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <style>
-        #backbtn{
-            padding: 5px;
-            font-size: 15px;
-            background-color: blue;
-            color: aliceblue;
-            border-radius: 5px;
-            float: left;
-        }
+        body{
+        background-color: aqua;
+    
+    }
 
-        #logoutbtn{
-            padding: 5px;
-            font-size: 15px;
-            background-color: blue;
-            color: aliceblue;
-            border-radius: 5px;
-            float: right;
-        }        
-    </style>
+    #headerSection{
+        padding: 5px;
+    }
 
-     <button id="backbtn">Back</button> 
-    <button id="logoutbtn">Logout</button>
-    <center><h1>Online Voting system</h1></center>
+    #headerSection h1{
+        font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    }
+
+    #backbtn{
+        padding: 5px;
+        font-size: 15px;
+        background-color: blue;
+        color: aliceblue;
+        border-radius: 5px;
+        float: left;
+    }
+
+    #logoutbtn{
+        padding: 5px;
+        font-size: 15px;
+        background-color: blue;
+        color: aliceblue;
+        border-radius: 5px;
+        float: right;
+    }  
+
+    </style> 
+    
+
+    <div id="mainSection">
+        <center>
+    <div id="headerSection">
+        <button id="backbtn">Back</button> 
+        <button id="logoutbtn">Logout</button>
+        <h1>Online Voting system</h1>
+    </div>
+</center>
     <hr>
-    <div class="profile"></div>
+    <div class="profile">
+        <img src="../uplords/<?php echo $userdata['photo']?>" height="200" width= "200"><br><br>
+        <b>Name:</b><?php echo $userdata['Name']?><br><br>
+        <b>Email:</b><?php echo $userdata['Email']?><br><Br>
+        <b>Mobile:</b><?php echo $userdata['number']?><br><br>
+        <b>Status:</b><?php echo $userdata['status']?>
+    </div>
     <div class="group"></div>
+    </div>
 </body>
 </html>
