eba2bf6c225631a431ab58bc4a89a32e87e155f0
Sourav Ghosh <souravghosh007980@gmail.com>
2023-11-05 19:57:06 +0530

    backend work

--- Routes/dashboard.php
+++ Routes/dashboard.php
@@ -5,6 +5,14 @@
     }
 
     $userdata = $_SESSION['userdata'];
+    $groupsdata = $_SESSION['groupsdata'];
+
+    if($userdata['status']==0){
+        $status= '<b style="color:red">Not voted</b>';
+    }
+    else{
+        $status= '<b style="color:green">voted</b>';
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -35,6 +43,7 @@
         color: aliceblue;
         border-radius: 5px;
         float: left;
+        margin : 15px;
     }
 
     #logoutbtn{
@@ -44,7 +53,34 @@
         color: aliceblue;
         border-radius: 5px;
         float: right;
+        margin : 15px;
     }  
+
+    #profile{
+        background-color: white;
+        width: 30%;
+        padding: 20px;
+        float: left;
+    }
+
+    #group{
+        background-color: white;
+        width: 60%;
+        padding: 20px;
+        float: right;
+    }
+    #votebtn{
+        padding: 5px;
+        font-size: 15px;
+        background-color: blue;
+        color: aliceblue;
+        border-radius: 5px;
+    }
+
+    #mainpannel{
+        padding: 10px;
+    }
+
 
     </style> 
     
@@ -58,14 +94,39 @@
     </div>
 </center>
     <hr>
-    <div class="profile">
-        <img src="../uplords/<?php echo $userdata['photo']?>" height="200" width= "200"><br><br>
+    <div id="mainpannel">
+    <div id="profile">
+        <center><img src="../uplords/<?php echo $userdata['photo']?>" height="150" width= "150"><br><br></center>
         <b>Name:</b><?php echo $userdata['Name']?><br><br>
         <b>Email:</b><?php echo $userdata['Email']?><br><Br>
         <b>Mobile:</b><?php echo $userdata['number']?><br><br>
-        <b>Status:</b><?php echo $userdata['status']?>
+        <b>Status:</b><?php echo $status?>
     </div>
-    <div class="group"></div>
+    <div id="group">
+        <?php
+            if($_SESSION['groupsdata']){
+                for($i=0; $i<count($groupsdata); $i++){
+                    ?>
+                    <div>
+                        <img style= "float: right" src="../uplords/<?php echo $groupsdata[$i]['photo']?>" height="100" width="100">
+                        <b>Group Name:</b><?php echo $groupsdata[$i]['Name']?><br><br>
+                        <b>Votes:</b><?php echo $groupsdata[$i]['votes']?> <br><br>
+                        <form action="vote.php" method="POST">
+                            <input type="hidden" name="gvotes" value=<?php echo $groupsdata[$i]['votes']?>>
+                            <input type="hidden" name="gid" value=<?php echo $groupsdata[$i]['S.no']?>>
+                            <input type="submit" name="votebtn" value="vote" id="votebtn">
+                        </form>
+                    </div>
+                    <hr>
+                    <?php
+                }
+            }
+            else{
+
+            }
+        ?>
+        </div>
+    </div>
     </div>
 </body>
 </html>
